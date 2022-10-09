35fb24ce7198e2a81adb8bcde371ab9fcb879924
RUBIN KUMAR K <91709659+rubinofficial-78@users.noreply.github.com>
2022-10-09 07:48:56 +0530

    Two Sum

--- MICROSOFT/INCLASS ASSIGNMENTS/WEEK 1/Two Sum.java
+++ MICROSOFT/INCLASS ASSIGNMENTS/WEEK 1/Two Sum.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int res[] = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int len = nums.length;
+        for(int i = 0 ; i < len ; i++){
+            if(map.containsKey(target-nums[i])){
+                res[1] = i;
+                res[0] = map.get(target-nums[i]);
+                return res;
+            }
+            map.put(nums[i], i);
+        }
+        return res;
+    }
+}
